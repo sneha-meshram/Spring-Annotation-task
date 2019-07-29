dec66d9f161778aa644a9840345823adfc921428
sneha <sneha.meshram@cgi.com>
2019-07-29 18:30:44 +0530

    BeanLifeCycle task updated

--- src/main/java/com/stackroute/MovieDemo.java
+++ src/main/java/com/stackroute/MovieDemo.java
@@ -21,24 +21,43 @@ public class MovieDemo {
 
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ConfigClass.class);
 
-        Movie movie1=context.getBean("movie1",Movie.class);
+     //   Movie movie1=context.getBean("movie1",Movie.class);
       //  Movie movie2=context.getBean("movie1",Movie.class);
-        System.out.println(movie1.getActors());
+      //  System.out.println(movie1.getActors());
      //   System.out.println(movie1==movie2);
 
-        Actors actor=context.getBean("actor",Actors.class);
-        System.out.println(actor);
+     //   Actors actor=context.getBean("actor",Actors.class);
+     //   System.out.println(actor);
 
-        Actors actors1=context.getBean("actors1",Actors.class);
-        System.out.println(actors1);
+      //  Actors actors1=context.getBean("actors1",Actors.class);
+      //  System.out.println(actors1);
 
-        Actors actors2=context.getBean("actors2",Actors.class);
-        System.out.println(actors2);
-        System.out.println(actors1==actors2);
+     //   Actors actors2=context.getBean("actors2",Actors.class);
+      //  System.out.println(actors2);
+     //   System.out.println(actors1==actors2);
 
-        ConfigurableApplicationContext context1=new AnnotationConfigApplicationContext(ConfigClass.class);
-        BeanLifecycleDemoBean demoBean=(BeanLifecycleDemoBean) context1.getBean("beanLifecycleDemoBean");
+     //   ConfigurableApplicationContext context1=new AnnotationConfigApplicationContext(ConfigClass.class);
+        BeanLifecycleDemoBean demoBean=(BeanLifecycleDemoBean) context.getBean("beanLifecycleDemoBean");
+
+        BeanPostProcessor demoBean1=context.getBean("beanPostProcessor", BeanPostProcessorDemoBean.class);
+
         context.close();
+
+
+
+
+
+
+//        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+//       // Actors actors=context.getBean("actors",Actors.class);
+//        Movie movie1=context.getBean("movie1",Movie.class);
+//        System.out.println(movie1.getActors());
+//
+//
+//        //calling method for BeanLifecycleDemoBean class.
+
+//        //calling method of BeanPostProcessor class.
+//        BeanPostProcessor beanPostProcessor=context1.getBean("beanPostProcessorDemoBean", BeanPostProcessorDemoBean.class);
 
     }
 }
--- src/main/java/com/stackroute/domain/ConfigClass.java
+++ src/main/java/com/stackroute/domain/ConfigClass.java
@@ -1,6 +1,8 @@
 package com.stackroute.domain;
 
 import com.stackroute.demo.BeanLifecycleDemoBean;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
+//import jdk.tools.jlink.internal.PostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,18 +29,18 @@ public class ConfigClass {
         return actors;
     }
 
-    @Bean
-    public Actors actors1() {
-
-        Actors actors1 = new Actors("shahrukh","male",45);
-        return actors1;
-    }
-    @Bean
-    public Actors actors2() {
-
-        Actors actors2 = new Actors("aamir","male",65);
-        return actors2;
-    }
+//    @Bean
+//    public Actors actors1() {
+//
+//        Actors actors1 = new Actors("shahrukh","male",45);
+//        return actors1;
+//    }
+//    @Bean
+//    public Actors actors2() {
+//
+//        Actors actors2 = new Actors("aamir","male",65);
+//        return actors2;
+//    }
 
     @Bean(name="beanLifecycleDemoBean")
     public BeanLifecycleDemoBean demoBean(){
@@ -47,6 +49,10 @@ public class ConfigClass {
      return bean;
     }
 
-
+    @Bean(name="beanPostProcessor")
+    public BeanPostProcessorDemoBean demoBean1() {
+        BeanPostProcessorDemoBean bean1 = new BeanPostProcessorDemoBean();
+        return bean1;
+    }
 
 }
